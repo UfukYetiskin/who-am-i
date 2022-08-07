26ff6afb0b0db9ab1144a864f1e56b74ccaa5115
uyetiskin <ufukyetiskin@hotmail.com>
2022-08-07 17:58:26 +0300

    home page created

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,16 +4,24 @@ import {useEffect , useState} from 'react'
 function Home() {
   const [pictures, setPictures] = useState([])
 
-  // useEffect(() => {
-  //   fetch('https://www.instagram.com/harut__marut/?__a=1')
-  //   .then(response => response.json())
-  //   .then(response => console.log(response))
-  //   .catch(err => console.error(err));
-  // })
+
 
   return (
     <div>
-      Will be here pictures
+      <div>
+        <img style={{width : '30%', margin : '10%', boxShadow :'10px 10px 10px 5px #ccc'}} src="https://www.linkpicture.com/q/WhatsApp-Image-2022-08-03-at-15.24.50.jpeg"  alt=''/>
+        <img style={{width : '20%', margin : '5%', boxShadow :'10px 10px 10px 5px #ccc'}} src='https://www.linkpicture.com/q/WhatsApp-Image-2022-08-03-at-15.37.04.jpeg' alt='' />
+      </div>
+      <div>
+        <img style={{width : '20%', margin : '5%', boxShadow :'10px 10px 10px 5px #ccc'}} src="https://www.linkpicture.com/q/WhatsApp-Image-2022-08-03-at-15.36.59.jpeg"  alt=''/>
+        <img style={{width : '30%', margin : '10%', boxShadow :'10px 10px 10px 5px #ccc'}} src='https://www.linkpicture.com/q/WhatsApp-Image-2022-08-03-at-16.26.08.jpeg' alt='' />
+      </div>
+      <div>
+        <img style={{width : '50%', margin : '2%', boxShadow :'10px 10px 10px 5px #ccc'}} src="https://www.linkpicture.com/q/WhatsApp-Image-2022-08-03-at-15.36.56-1.jpeg"  alt=''/>
+        <img style={{width : '30%', margin : '5%', boxShadow :'10px 10px 10px 5px #ccc'}} src='https://www.linkpicture.com/q/WhatsApp-Image-2022-08-03-at-15.37.05.jpeg' alt='' />
+      </div>
+      <div>
+        <img style={{width : '30%', margin : '2%', boxShadow :'10px 10px 10px 5px #ccc'}} src="https://www.linkpicture.com/q/WhatsApp-Image-2022-08-03-at-15.36.53.jpeg"  alt=''/>      </div>
     </div>
   )
 }
